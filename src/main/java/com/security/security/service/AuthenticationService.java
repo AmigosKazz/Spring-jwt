5b8d6018c378c942de2cf0e4195297604560647f
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-03-25 14:34:17 +0300

    security configuration

--- src/main/java/com/security/security/service/AuthenticationService.java
+++ src/main/java/com/security/security/service/AuthenticationService.java
@@ -1,14 +1,19 @@
 package com.security.security.service;
 
 import com.security.security.repo.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public AuthenticationService(UserRepository userRepository) {
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
 
